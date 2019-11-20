804b59c7deedb9808399d58ebd1324b7e36158a8
sun <1075067280@qq.com>
2019-11-20 20:54:43 +0800

    1

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  components:{
+    App
+  },
+  template:'<App/>'
+  // render: h => h(App),
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 运行时包含编译器的版本
+  runtimeCompiler: true,
+}
