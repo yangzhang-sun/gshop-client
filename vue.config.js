3f3b5d6f00345a4b0e56bed73d9935bbcdd3e506
sun <1075067280@qq.com>
2019-11-21 00:09:31 +0800

    router

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
+const path = require('path')
+const px2rem = require('postcss-px2rem')
+
+// 配置postcs-px2rem
+const postcss = px2rem({
+  remUnit: 37.5   // 设计稿等分后的rem值   750/10 = 75
+})
+
+function resolve (dir) {
+  return path.join(__dirname,  dir)
+}
 module.exports = {
   // 运行时包含编译器的版本
   runtimeCompiler: true,
+  // 关闭ESLint编译
+  lintOnSave: false,
+
+  configureWebpack : {
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@': resolve('src'),
+        'components': resolve('src/components'),
+      }
+    },
+  },
+  css: { // 添加postcss配置
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          postcss
+        ]
+      }
+    }
+  },
 }
