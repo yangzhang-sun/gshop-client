9d6e997790e142dcc6eb4f5c21474bcd88f3f1d3
sun <1075067280@qq.com>
2019-11-21 09:39:39 +0800

    footer

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,18 +1,32 @@
 <template>
-  <div>
-    <div @click="goPath('/Home')">
+  <div id="footerGuideContainer">
+    <div class="guideItem" @click="goPath('/Home')">
+      <span>
+        <i class="iconfont icon-index"></i>
+      </span>
       <span>首页</span>
     </div>
-    <div @click="goPath('/Sort')">
+    <div class="guideItem" @click="goPath('/Sort')">
+      <span>
+        <i class="iconfont icon-leimupinleifenleileibie"></i>
+      </span>
       <span>分类</span>
     </div>
-    <div @click="goPath('/Flush')">
-      <span>劲爆清仓</span>
+    <div class="guideItem" @click="goPath('/Flush')">
+      <span>
+        <img class="iconfont flush" src="https://img09.jiuxian.com/bill/2018/0528/56760d41e3e745afa14e3cea1713631c.png">
+      </span>
     </div>
-    <div @click="goPath('/Cart')">
+    <div class="guideItem" @click="goPath('/Cart')">
+      <span>
+        <i class="iconfont icon-icongouwuche1"></i>
+      </span>
       <span>购物车</span>
     </div>
-    <div @click="goPath('/Login')">
+    <div class="guideItem" @click="goPath('/Login')">
+      <span>
+        <i class="iconfont icon-wodedangxuan1"></i>
+      </span>
       <span>我的酒仙</span>
     </div>
   </div>
@@ -29,6 +43,30 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+@import "../../common/stylus/mixins.styl"
+  #footerGuideContainer
+    position fixed
+    left 0
+    bottom 0
+    width 100%
+    height 50px
+    border-top 1px solid #eee
+    box-sizing border-box
+    display flex
+    background #eee
+    .guideItem
+      width 25%
+      height 100%
+      padding-bottom 6px
+      text-align center
+      display flex
+      flex-direction column
+      span
+        i
+        &:first-child
+          margin 6px 0
+      .flush
+        width 42px
+        height 42px
  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'lib-flexible/flexible'
 
 import router from './router'
 Vue.config.productionTip = false
