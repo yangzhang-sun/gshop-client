66451d5e4a20e11bc9c317b385b4cb8a63c9e790
king <1441922245@qq.com>
2019-11-21 10:08:02 +0800

    dev

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    Login
+  <div class="g-loginContainer">
+   <div class="g-loginInner">
+     <div class="g-login_header">
+       <h2 class="g-login_logo">用户登录</h2>
+       <a href=""></a>
+       <a href="javascript:;"  >账号登录</a>
+       <a href="javascript:;" >手机动态密码登录</a>
+     </div>
+     <div class="g-login_content">
+
+     </div>
+   </div>
   </div>
 </template>
 
@@ -10,6 +20,26 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .g-loginContainer
+    width 100%
+    height 100%
+    background #de4943
+     .g-loginInner
+      padding-top 60px
+      width 80%
+      margin 0 auto
+      .g-login_header
+        height 40px
+        background-color :#de4943
+        color : #fff
+        .g-login_logo
+          font-size 40px
+          font-weight bold
+          background-color :#de4943 
+
+    
+    
+
 
  
 </style>
