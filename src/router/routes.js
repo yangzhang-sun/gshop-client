eade81023ae016c888e1604de9aad7fbacd155f7
tingtailouxie <3231739433@qq.com>
2019-11-22 08:49:10 +0800

    11

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,100 +9,62 @@ import Cart from '../pages/Cart/Cart.vue'
 import Home from '../pages/Home/Home.vue'
 import Flush from '../pages/Flush/Flush.vue'
 import Sort from '../pages/Sort/Sort.vue'
-<<<<<<< HEAD
 import CartList from '../components/CartList/CartList.vue'
 import CartEmply from '../components/CartEmply/CartEmply.vue'
 import WhileJiu from '../pages/Home/WhiteJiu/WhiteJiu.vue'
+import SortList from '../pages/SortList/SortList.vue'
+import Purchase from '../pages/Purchase/Purchase.vue'
+
 
 export default[
   {
-    path:'/Login',
+    path:'/login',
     component:Login,
     meta: {
       isShowFooterGuide: true
     }
   },
   {
-    path:'/Cart',
+    path:'/cart',
     component:Cart,
     children:[
       {
-        path:'/Cart/CartList',
+        path:'/cart/cartList',
         component:CartList,
       },
       {
-        path:'/Cart/CartEmply',
+        path:'/cart/cartEmply',
         component:CartEmply,
       }
     ]
   },
   {
-    path:'/Home',
+    path:'/home',
     component:Home,
     meta: {
       isShowFooterGuide: true
     }
   },
   {
+    path: '/purchase',
+    component: Purchase
+  },
+  {
     path:'/whileJiu',
     component:WhileJiu,
   },
   {
-    path:'/Flush',
+    path:'/flush',
     component:Flush,
     meta: {
       isShowFooterGuide: true
     }
   },
   {
-    path:'/Sort',
+    path:'/sort',
     component:Sort,
     meta: {
       isShowFooterGuide: true
     }
-=======
-import SortList from '../pages/SortList/SortList.vue'
-import Purchase from '../pages/Purchase/Purchase.vue'
-
-export default[
-  {
-    path:'/login',
-    component:Login
-  },
-  {
-    path:'/cart',
-    component:Cart
-  },
-  {
-    path:'/home',
-    component:Home
-    // meta:{//只有一个页面需要底部导航
-    //   isShowFooterGuide:true
-    // }
-  },
-  {
-    path: '/Purchase',
-    component: Purchase
-  },
-  {
-    path:'/flush',
-    component:Flush
-  },
-  {
-    path:'/sort',
-    component:Sort
->>>>>>> 764a6d2fab0870155e8e3979942c11fa62a85c24
-  },
-  {
-    path:'/sortList',
-    component:SortList
-  },
-  {
-    path:'/',
-<<<<<<< HEAD
-    redirect:'/whileJiu'
-=======
-    redirect: '/sort'
->>>>>>> 764a6d2fab0870155e8e3979942c11fa62a85c24
   }
 ]
